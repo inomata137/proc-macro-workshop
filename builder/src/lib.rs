f666ed69f6bc67f9e5c3b5f659e1061fe3049b57
inomata137 <inomata2016@icloud.com>
2024-11-03 16:09:34 +0900

    02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,39 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
-    TokenStream::new()
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let DeriveInput {
+        ident,
+        vis,
+        generics,
+        ..
+    } = input;
+
+    let builder_ident = format_ident!("{}Builder", ident);
+
+    let expanded = quote! {
+        impl #generics #ident #generics {
+            #vis fn builder() -> #builder_ident #generics {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        #vis struct #builder_ident #generics {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+    };
+
+    TokenStream::from(expanded)
 }
