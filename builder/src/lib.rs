42c85837ed0d1c929f6f2ab597bd51c698ef4a8e
inomata137 <inomata2016@icloud.com>
2024-11-03 16:58:09 +0900

    03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::*;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -10,28 +10,66 @@ pub fn derive(input: TokenStream) -> TokenStream {
         ident,
         vis,
         generics,
+        data,
         ..
     } = input;
 
     let builder_ident = format_ident!("{}Builder", ident);
 
+    let fields = match data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(FieldsNamed { named, .. }),
+            ..
+        }) => named,
+        _ => {
+            return quote! {
+                compile_error!("Builder derive only works on structs with named fields");
+            }
+            .into();
+        }
+    };
+
+    let builder_defaults = fields.iter().map(|field| {
+        let field_name = &field.ident;
+        quote! {
+            #field_name: None,
+        }
+    });
+
+    let builder_field_definitions = fields.iter().map(|field| {
+        let field_name = &field.ident;
+        let ty = &field.ty;
+        quote! {
+            #field_name: Option<#ty>,
+        }
+    });
+
+    let builder_setters = fields.iter().map(|field| {
+        let field_name = &field.ident;
+        let ty = &field.ty;
+        quote! {
+            #vis fn #field_name(&mut self, #field_name: #ty) -> &mut Self {
+                self.#field_name = Some(#field_name);
+                self
+            }
+        }
+    });
+
     let expanded = quote! {
         impl #generics #ident #generics {
             #vis fn builder() -> #builder_ident #generics {
                 #builder_ident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#builder_defaults)*
                 }
             }
         }
 
         #vis struct #builder_ident #generics {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#builder_field_definitions)*
+        }
+
+        impl #generics #builder_ident #generics {
+            #(#builder_setters)*
         }
     };
 
